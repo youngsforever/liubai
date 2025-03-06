15cbdc357ad0a8fb7795ce888881c1fe2147d6e2
yenche123 <tsuiyenche@outlook.com>
2025-03-06 15:07:38 +0800

    feat: no ai icon

--- liubai-frontends/liubai-web/src/components/level1/thread-list/thread-card/tc-topbar/tools/useTcTopbar.ts
+++ liubai-frontends/liubai-web/src/components/level1/thread-list/thread-card/tc-topbar/tools/useTcTopbar.ts
@@ -28,6 +28,7 @@ export function useTcTopbar(
     const t = td.value
     if(t.pinStamp) return true
     if(t.stateId && t.stateShow) return true
+    if(!t.aiReadable || t.aiReadable === "N") return true
     if(t.storageState === `LOCAL` || t.storageState === `ONLY_LOCAL`) return true
     if(aiCharacterUrl.value) return true
     return false
@@ -36,6 +37,7 @@ export function useTcTopbar(
   const cloudOffPlacement = computed<TooltipPlacement>(() => {
     const t = td.value
     if(t.stateShow) return `bottom`
+    if(t.aiCharacter) return `bottom`
     return `bottom-end`
   })
 
