fa0cb3190428619799f3e8d78ad9fb48a6f16949
yenche123 <tsuiyenche@outlook.com>
2025-06-03 12:52:09 +0800

    feat: uploading file for avatar

--- liubai-frontends/liubai-web/src/pages/level4/cropper-page/cropper-content/tools/useCropperContent.ts
+++ liubai-frontends/liubai-web/src/pages/level4/cropper-page/cropper-content/tools/useCropperContent.ts
@@ -6,6 +6,12 @@ import type { CcProps } from "./types";
 import type { CropperResult } from "vue-advanced-cropper"
 import cui from "~/components/custom-ui";
 import imgHelper from "~/utils/files/img-helper";
+import type { FileSetAPI } from "~/requests/req-types"
+import type { LiuImageStore } from "~/types"
+import APIs from "~/requests/APIs"
+import liuReq from "~/requests/liu-req"
+import { uploadViaQiniu } from "~/utils/cloud/upload-tasks/tools/upload-via-qiniu"
+import ider from "~/utils/basic/ider";
 
 
 let isLoading = false
@@ -43,7 +49,7 @@ export function useCropperContent(
     canvas.toBlob(async (blob) => {
       if(!blob) return
       compressFile(blob)
-    })
+    }, "image/jpeg")
   })
 
   return {
@@ -52,9 +58,11 @@ export function useCropperContent(
   }
 }
 
-
-function showCropperLoading() {
-  cui.showLoading({ title_key: "tip.hold_on" })
+function showCropperLoading(
+  title_key: string = "tip.hold_on",
+) {
+  isLoading = true
+  cui.showLoading({ title_key })
 }
 
 function hideCropperLoading() {
@@ -72,4 +80,65 @@ async function compressFile(
     convertSize: 32 * 1024,
   })
   console.log("blobs: ", blobs)
+  const firstBlob = blobs[0]
+  if(!firstBlob) return
+  uploadFile(firstBlob)
+}
+
+async function uploadFile(
+  blob: Blob,
+) {
+  // 1. get token
+  const url = APIs.UPLOAD_FILE
+  const param: FileSetAPI.Param = { 
+    operateType: "get-upload-token",
+    purpose: "avatar",
+  }
+  const tokenRes = await liuReq.request<FileSetAPI.Res_UploadToken>(url, param)
+  console.log("tokenRes: ", tokenRes)
+  if (tokenRes.code !== "0000" || !tokenRes.data) {
+    console.warn("获取上传 token 失败", tokenRes)
+    hideCropperLoading()
+    return
+  }
+
+  // 2. create file with id
+  const file = new File([blob], "avatar.jpg", { type: "image/jpeg" })
+  const arrBuf = await file.arrayBuffer()
+  const imgStore: LiuImageStore = {
+    id: ider.createImgId(),
+    name: file.name,
+    lastModified: file.lastModified,
+    mimeType: file.type,
+    arrayBuffer: arrBuf,
+    h2w: "1",
+    size: file.size,
+  }
+
+  // 3. show uploading
+  showCropperLoading("common.uploading")
+
+  // 4. upload file
+  await uploadViaQiniu(
+    tokenRes.data, 
+    [imgStore],
+    (fileId, res) => {
+      const cloud_url = res?.data?.cloud_url
+      if (res.code === "0000" && cloud_url) {
+        uploadAvatar(imgStore, cloud_url)
+      }
+      else {
+        hideCropperLoading()
+      }
+    }
+  )
+}
+
+async function uploadAvatar(
+  imgStore: LiuImageStore,
+  cloud_url: string,
+) {
+  imgStore.cloud_url = cloud_url
+  
+  
 }
