ac2cd0adb7570f8a1c56032077c087fb3964d93a
yenche123 <tsuiyenche@outlook.com>
2025-09-07 15:55:31 +0800

    fix: bindingStatus

--- liubai-frontends/liubai-weixin/miniprogram/packageB/pages/task-detail/task-detail.ts
+++ liubai-frontends/liubai-weixin/miniprogram/packageB/pages/task-detail/task-detail.ts
@@ -231,7 +231,7 @@ Component({
       this.toUpdateShareMenu()
 
       // 4.3 check out binding status
-      if(bind4.detail.remindStr && justOnLoad) {
+      if(bind4.detail.remindStr && !this.data.bindingStatus) {
         this.handleBindingStatus()
       }
 
@@ -417,19 +417,21 @@ Component({
       LiuApi.vibrateShort({ type: "light" })
       
       const bs = this.data.bindingStatus
-      if(this.data.qrCodePicUrl && bs === "unfollowed") {
-        this.setData({ openBindingPopup: true })
+      console.log("bs: ", bs)
+
+      if(bs === "unfollowed") {
+        if(this.data.qrCodePicUrl) {
+          this.setData({ openBindingPopup: true })
+        }
         return
       }
 
-      if(bs === "followed") {
-        LiuUtil.showCustomModal({
-          title_key: "task-detail.remind_1",
-          content_key: "task-detail.remind_2",
-          showCancel: false,
-          confirm_key: "shared.ok",
-        })
-      }
+      LiuUtil.showCustomModal({
+        title_key: "task-detail.remind_1",
+        content_key: "task-detail.remind_2",
+        showCancel: false,
+        confirm_key: "shared.ok",
+      })
     },
 
     onTapShare() {
